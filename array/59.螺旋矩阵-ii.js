b41468fae44681a0e411d1dc45aac6941503261d
${{ secrets.GITHUBUSERNAME }} <1436159527@qq.com>
2025-02-19 09:39:30 +0800

    feat(array): 生成螺旋矩阵
    - 实现了生成 n x n 螺旋矩阵的函数 generateMatrix
    - 使用四个边界变量来控制螺旋的方向和范围
    - 从外向内逐层填充矩阵，最后设置中心点的值

--- array/59.螺旋矩阵-ii.js
+++ array/59.螺旋矩阵-ii.js
@@ -10,7 +10,47 @@
  * @return {number[][]}
  */
 var generateMatrix = function(n) {
-    
+    let top = 0
+    let right = n - 1
+    let bottom = n - 1
+    let left = 0
+    let res = Array.from({length: n}, () => Array(n).fill(0));
+    let k = 1
+
+    while (k < (n * n)) {
+        // 左到右
+        for (let i = left; i < right; i++) {
+            res[top][i] = k
+            k++
+        }
+        // 上到下
+        for (let i = top; i < bottom; i++) {
+            res[i][right] = k
+            k++
+        }
+        // 右到左
+        for (let i = right; i > left; i--) {
+            res[bottom][i] = k
+            k++
+        }
+        // 下到上
+        for (let i = bottom; i > top; i--) {
+            res[i][left] = k
+            k++
+        }
+
+        // 向内缩
+        top++
+        right--
+        bottom--
+        left++
+    }
+
+    // 中心点
+    res[left][right] = n * n
+
+    return res
+
 };
 // @lc code=end
 
